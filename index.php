e039c7da45e712c9cc0bdab5925d28baf980060a
Nafion Antoy <antoynafion@gmail.com>
2022-10-24 16:51:47 +0200

    Changement de l'extension index.html en index.php, ajout du dossier 'framware' et du fichier 'porfolio.png'. ne contient encore que la page d'acceuil.

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="fr-FR">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Nafion Antoy</title>
+    <link rel="stylesheet" href="style.css" />
+    <link rel="shortcut icon" href="img/logo.png" type="image/x-icon" />
+  </head>
+  <body>
+    <header>
+      <div class="top-header">
+        <p>
+          <img id="logo" src="img/logo2.png" alt="Logo" />
+        </p>
+        <nav>
+          <ul class="nav_header">
+            <li><a href="#">Linkedin</a></li>
+            <li><a href="#">Projets</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">Objectifs</a></li>
+            <li><a href="#">Passions</a></li>
+          </ul>
+        </nav>
+      </div>
+      <hr />
+      <section class="section_header">
+        <div class="identite">
+          <h1>Nafion Antoy</h1>
+          <p>Développeur front Junior en autodidacte depuis un an.</p>
+          <p>Qui suis-je ?</p>
+        </div>
+        <img id="baniere" src="img/baniere.jpg" alt="banière" />
+      </section>
+      <section class="section_footer">
+        <footer>
+          <h2>Moyen de formation</h2>
+          <div class="contenu_footer">
+            <div>
+            <p>Openclassrooms</p>
+            <p>Udemy</p>
+            <p>Mimo_dev</p>
+            <p>Dunod</p>
+          </div>
+          <div>
+            <p>W3Schools</p>
+            <p>MDN Web Docs</p>
+            <nav>
+              <ul class="nav_footer">
+                <li><a href="#">Linkedin</a></li>
+                <li><a href="#">Projets</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Objectifs</a></li>
+                <li><a href="#">Passions</a></li>
+              </ul>
+            </nav>
+        </footer>
+
+        <p class="video_container">
+          <video class="video_header" src="#" controls poster="#"></video>
+        </p>
+      </section>
+    </header>
+  </body>
+</html>
